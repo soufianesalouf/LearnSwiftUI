849eccfe36b0bb4ea3286ed1b60f57ca5f1875bb
Soufiane Salouf <soufiane.salouf@ibm.com>
2020-05-06 01:19:35 +0000

    [MASTER] NEW - Init base project

---
+++ LearnSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnSwiftUI
+//
+//  Created by Soufiane Salouf on 5/6/20.
+//  Copyright © 2020 Soufiane Salouf. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

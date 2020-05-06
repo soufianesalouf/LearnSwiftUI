7d29ea332ec59755e69e627b741e778f869e78d1
Soufiane Salouf <soufiane.salouf@ibm.com>
2020-05-06 02:15:55 +0000

    [WarmUP] NEW - My First code in SwiftUI

--- LearnSwiftUI/ContentView.swift
+++ LearnSwiftUI/ContentView.swift
@@ -9,8 +9,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Constants
+    let gender = ["Male", "Female"]
+    
+    // Mutating var
+    @State private var countTaps = 2
+    @State private var name = ""
+    @State private var defaultGender = "Male"
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section {
+                    
+                    // Text Field binded with property name
+                    TextField("Enter your name", text: $name)
+                    
+                    // Show Simple Text
+                    Text("The name entered is : \(name)")
+                    Text("Tap on the button to increase the value")
+                    Text("\(countTaps)")
+                    
+                    Picker("Gender", selection: $defaultGender) {
+                        ForEach(0 ..< gender.count) {
+                            Text(self.gender[$0])
+                        }
+                    }
+                }
+                
+                Group {
+                    Button("Tap here") {
+                        self.countTaps += 10
+                    }
+                }
+            }
+        .navigationBarTitle("SwiftUI")
+        }
     }
 }
 
